08ea17a116aea7014dc4417533c538add5215822
PIYUSH <96675867+Piyush0922@users.noreply.github.com>
2023-04-08 19:00:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 (function(){
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
-    let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
+    let clear = document.querySelector('.clear');
+    let equal = document.querySelector('.equal');
 
     buttons.forEach(function(button){
         button.addEventListener('click',function(e){
